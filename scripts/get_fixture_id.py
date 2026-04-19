1360e460b2a901838465b8aac611e7b3c168c8f7
Zhaokai Wang <wangzhaokai@sjtu.edu.cn>
2026-04-19 16:15:50 +0100

    test two matches

--- scripts/get_fixture_id.py
+++ scripts/get_fixture_id.py
@@ -5,17 +5,38 @@ load_dotenv()  # 自动加载 .env 里的所有变量
 import http.client
 
 
-conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+def get_id():
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
 
-headers = {
-    'x-apisports-key': os.getenv("API_FOOTBALL_KEY")
-    }
+    headers = {
+        'x-apisports-key': os.getenv("API_FOOTBALL_KEY")
+        }
 
-conn.request("GET", "/fixtures?date=2026-04-28&league=2&season=2025", headers=headers)
+    conn.request("GET", "/fixtures?date=2026-04-19&league=39&season=2025", headers=headers)
 
-res = conn.getresponse()
-data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-print(data.decode("utf-8"))
+    print(data.decode("utf-8"))
 
-# bayern madrid: 1534911
+    # bayern madrid: 1534911
+
+
+def get_fixture(id):
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+    headers = {
+        'x-apisports-key': os.getenv("API_FOOTBALL_KEY")
+        }
+
+    conn.request("GET", f"/fixtures?id={id}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    # get_id()
+    get_fixture(1379295)
